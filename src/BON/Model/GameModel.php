f8d295f2cab165ee67495814d9c2af5c075c528b
Kristian Epps <kristian@bbc.co.uk>
2013-10-26 18:11:30 +0100

    Added (untested) code to produce a maze grid

--- src/BON/Model/GameModel.php
+++ src/BON/Model/GameModel.php
@@ -12,14 +12,131 @@ abstract class GameModel
 		$this->name = $name;
 	}
 
-	public function join($player)
+	protected function has_player($player)
 	{
-		$this->players[] = $player;
+		foreach($this->players as $current_player)
+			if($current_player->equals($new_player));
+				return true;
+
+		return false;
+	}
+
+	public function join($new_player)
+	{
+		if(!$this->has_player())
+			$this->players[] = $new_player;
 	}
 
 }
 
 class MazeModel extends GameModel
 {
-	$maze;
+	protected $grid;
+	protected $start;
+	protected $end;
+
+	public function generate_maze($x_size, $y_size)
+	{
+		$wall_list = new Array();
+		$grid = new Array();
+
+		for(i = 0; i < $x_size; $i++)
+			for(j = 0; j < $y_size; $j++)
+				$grid[i][j] = "#";
+
+		$side = rand(1,4);
+
+		if($side == 1)
+			$this->start = (rand(0, $x_size-1), 0);
+		else if($side == 2)
+			$this->start = ($x_size-1, rand(0, $y_size-1));
+		else if($side == 3)
+			$this->start = (rand(0, $x_size-1), $y_size-1);
+		else if($side == 4)
+			$this->start = (0, rand(0, $y_size-1));
+
+		$current = $this->start;
+		$grid[$current[0]][$current[1]] = " ";
+
+		if($current[0]+1 < $x_size)
+			$wall_list[] = array($current[0]+1, $current[1]);
+
+		if($current[0]-1 > 0)
+			$wall_list[] = array($current[0]-1, $current[1]);
+
+		if($current[1]+1 < $y_size)
+			$wall_list[] = array($current[0], $current[1]+1);
+
+		if($current[1]-1 > 0)
+			$wall_list[] = array($current[0], $current[1]-1);
+
+		while(sizeof($wall_list) > 0)
+		{
+			$current_wall = rand(0, sizeof($wall_list));
+
+			$adjacent_spaces = 0;
+			$potential_spaces = Array();
+
+			if($wall_list[$current_wall][0]+1] < $x_size)
+				if($grid[$wall_list[$current_wall][0]+1][$wall_list[$current_wall][1]] == " ")
+					$adjacent_spaces += 1;
+				else
+					$potential_space[] = array($wall_list[$current_wall][0]+1, $wall_list[$current_wall][1]);
+
+			if($wall_list[$current_wall][0]-1] > 0)
+				if($grid[$wall_list[$current_wall][0]-1][$wall_list[$current_wall][1]] == " ")
+					$adjacent_spaces += 1;
+				else
+					$potential_spaces[] = array($wall_list[$current_wall][0]-1, $wall_list[$current_wall][1]);
+
+			if($wall_list[$current_wall][1]+1] < $y_size)
+				if($grid[$wall_list[$current_wall][0]][$wall_list[$current_wall][1]+1] == " ")
+					$adjacent_spaces += 1;
+				else
+					$potential_spaces[] = array($wall_list[$current_wall][0], $wall_list[$current_wall][1]+1);
+
+			if($wall_list[$current_wall][1]-1] > 0)
+				if($grid[$wall_list[$current_wall][0]][$wall_list[$current_wall][1]-1] == " ")
+					$adjacent_spaces += 1;
+				else
+					$potential_spaces[] = array($wall_list[$current_wall][0], $wall_list[$current_wall][1]-1);
+
+			if($adjacent_spaces == 1)
+			{
+				$new_space = rand(0, sizeof($potential_spaces));
+				$grid[$potential_spaces[$new_space][0]][$potential_spaces[$new_space][1]] = " ";
+
+				for($i = 0, $i < sizeof($potential_spaces); $i++)
+					if($i != $new_space)
+						$wall_list[] = $potential_spaces[$i];
+			}
+
+			if(sizeof($wall_list) == 1)
+				$this->end_point = $wall_list[$current_wall];
+
+			unset($wall_list[$current_wall]);
+		}
+
+		// TO-DO
+		// Generate End Point
+
+		$this->grid = $grid;
+		return $grid;
+
+	}
+
+	public function get_start()
+	{
+		return $this->start;
+	}
+
+	public function get_end()
+	{
+		return $this->end;
+	}
+
+	public function get_grid()
+	{
+		return $this->grid;
+	}
 }
--- src/BON/Model/PlayerModel.php
+++ src/BON/Model/PlayerModel.php
@@ -27,4 +27,24 @@ class Player
 	{
 		return send($message);
 	}
+
+	public function equals($player)
+	{
+		if(get_class($player) == get_class($this))
+			if($player->get_phone_numer() == $this->get_phone_numer())
+				return true;
+
+		return false;
+
+	}
+
+	public function get_phone_numer()
+	{
+		return $this->phone_number;
+	}
+
+	public function get_user_name()
+	{
+		return $this->user_name;
+	}
 }
