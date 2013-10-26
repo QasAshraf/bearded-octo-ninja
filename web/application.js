22bf489ba9c750a1f4a1aacad29b906a11360b4c
Joe Forshaw <joe.d.forshaw@gmail.com>
2013-10-26 16:33:08 +0100

    moved files to web

---
+++ web/application.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+  var stage = new Kinetic.Stage({
+    container: "game-container",
+    width: window.innerWidth,
+    height: (window.innerWidth / 100) * 40
+  });
+
+  var layer = new Kinetic.Layer();
+
+  var rect = new Kinetic.Rect({
+    x: 239,
+    y: 75,
+    width: 100,
+    height: 50,
+    fill: 'green',
+    stroke: 'black',
+    strokeWidth: 4
+  });
+
+  // add the shape to the layer
+  layer.add(rect);
+
+  // add the layer to the stage
+  stage.add(layer);
+
+  // setInterval(function() {
+  //   rect.move(1, 0);
+  //   stage.draw();
+  // }, 100);
+
+  var vector = [5,5];
+
+  console.log(parseInt($("canvas").height()));
+  
+  var anim = new Kinetic.Animation(function(frame) {
+    movePlayer(rect, vector);
+  }, layer);
+  anim.start();
+
+});
+
+function movePlayer(player, velocity) {
+
+  // Check top collision
+  if (player.getPosition().y <= 0) {
+    console.log("top");
+    velocity[1] *= -1;
+  }
+
+  // Check bottom collision
+  if (player.getPosition().y + player.getHeight() >= parseInt($("canvas").height())) {
+    console.log("bottom");
+    velocity[1] *= -1;
+  }
+
+  // Check left collision
+  if (player.getPosition().x <= 0) {
+    console.log("left");
+    velocity[0] *= -1;
+  }
+
+  // Check right collision
+  if (player.getPosition().x + player.getWidth() >= parseInt($("canvas").width())) {
+    console.log("right");
+    velocity[0] *= -1;
+  }
+
+  player.move(velocity[0], velocity[1]);
+
+}
