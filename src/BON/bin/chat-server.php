c6f9dc54ef3e501a3213ee5132747751357688f5
stephcook22 <stephcook22@gmail.com>
2013-10-26 21:18:12 +0100

    Added connection from clockwork to websocket server. requires composer update and testing ;)

--- src/BON/bin/chat-server.php
+++ src/BON/bin/chat-server.php
@@ -6,13 +6,26 @@ use BON\Chat;
 
     require dirname(__DIR__) . '/../../vendor/autoload.php';
 
-    $server = IoServer::factory(
-        new HttpServer(
-            new WsServer(
-                new Chat()
+    $loop   = React\EventLoop\Factory::create();
+
+    $chat = new Chat();
+
+    $context = new React\ZMQ\Context($loop);
+    $pull = $context->getSocket(ZMQ::SOCKET_PULL);
+    $pull->bind('tcp://127.0.0.1:5555'); // Binding to 127.0.0.1 means the only client that can connect is itself
+    $pull->on('message', array($chat, 'onTextReceived'));
+
+    $webSock = new React\Socket\Server($loop);
+    $webSock->listen(8080, '0.0.0.0'); // Binding to 0.0.0.0 means remotes can connect
+    $webServer = new Ratchet\Server\IoServer(
+        new Ratchet\Http\HttpServer(
+            new Ratchet\WebSocket\WsServer(
+                new Ratchet\Wamp\WampServer(
+                    $chat
+                )
             )
         ),
-        8080
+        $webSock
     );
 
-    $server->run();
+    $loop->run();
