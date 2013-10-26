8038757ccf0911b8ed1ac9d0c4cb0d51789ae3db
Joe Forshaw <joe.d.forshaw@gmail.com>
2013-10-26 18:43:49 +0100

    changed js to move and add players on ws message

--- web/application.js
+++ web/application.js
@@ -10,53 +10,58 @@ grid = [
 
 $(document).ready(function() {
 
-  // var conn = new WebSocket('ws://localhost:8080');
-  // conn.onopen = function(e) {
-  //     console.log("Connection established!");
-  // };
+  var conn = new WebSocket('ws://109.109.137.94:8080');
+  conn.onopen = function(e) {
+    console.log("Connection established!");
+  };
 
-  // conn.onmessage = function(e) {
-  //     console.log(e.data);
-  // };
+  conn.onmessage = function(e) {
+    console.log(e.data);
+    splitData = e.data.split(" ");
+    if (splitData[0] === "move") {
+      movePlayer(splitData[1], splitData[2], splitData[3]);
+    } else if (splitData[0] === "add") {
+      addPlayer();
+    }
+  };
+
+  conn.onclose = function(e) {
+    console.log("Connection closed!");
+  };
 
   stage = new Kinetic.Stage({
     container: "game-container",
     width: window.innerWidth,
-    height: (window.innerWidth / 100) * 40
+    height: (window.innerHeight / 100) * 80
   });
-
   layer = new Kinetic.Layer();
-
   players = [];
+  velocity = [5,5];
 
-  // add the layer to the stage
+  // Add the layer to the stage
   stage.add(layer);
 
+  // Add test players
   addPlayer();
-  setTimeout(addPlayer, 1000);
 
-  var vector = [5,5];
+  // Test move
+  setTimeout(function() {
+    movePlayer(players[0], 800, 500);
+  }, 1000);
 
   console.log(parseInt($("canvas").height()));
-  
-  var anim = new Kinetic.Animation(function(frame) {
-    for (var i = 0; i < players.length; i++) {
-      movePlayer(players[i], vector);
-    }
-  }, layer);
-  anim.start();
 
 });
 
 function addPlayer() {
   var newPlayer = new Kinetic.Rect({
-    x: 239,
-    y: 75,
-    width: 100,
+    x: 10,
+    y: 10,
+    width: 50,
     height: 50,
     fill: 'green',
     stroke: 'black',
-    strokeWidth: 4
+    strokeWidth: 0
   });
 
   players.push(newPlayer);
@@ -65,31 +70,29 @@ function addPlayer() {
   layer.add(newPlayer);
 }
 
-function movePlayer(player, velocity) {
+function movePlayer(player, x, y) {
 
-  // Check top collision
-  if (player.getPosition().y <= 0) {
-    console.log("top");
-    velocity[1] *= -1;
-  }
+  var anim = new Kinetic.Animation(function(frame) {
+      var distanceToX = player.getPosition().x - x;
+      var distanceToY = player.getPosition().y - y;
+      // console.log(distanceToX,distanceToY);
+      if (distanceToX > 0) {
+        player.move(velocity[0] * -1,0);
+      } else if (distanceToX < 0) {
+        player.move(velocity[0],0);
+      }
 
-  // Check bottom collision
-  if (player.getPosition().y + player.getHeight() >= parseInt($("canvas").height())) {
-    console.log("bottom");
-    velocity[1] *= -1;
-  }
+      if (distanceToY > 0) {
+        player.move(0, velocity[1] * -1);
+      } else if (distanceToY < 0) {
+        player.move(0, velocity[1]);
+      }
 
-  // Check left collision
-  if (player.getPosition().x <= 0) {
-    console.log("left");
-    velocity[0] *= -1;
-  }
-
-  // Check right collision
-  if (player.getPosition().x + player.getWidth() >= parseInt($("canvas").width())) {
-    console.log("right");
-    velocity[0] *= -1;
-  }
+      if (distanceToX === 0 && distanceToY === 0) {
+        this.stop();
+      }
+  }, layer);
+  anim.start();
 
   player.move(velocity[0], velocity[1]);
 
