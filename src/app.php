bbce9e311e9969f70023a9c31b98bb3aa2c29c07
Qas Ashraf <mail@qasashraf.co.uk>
2013-10-27 04:20:22 +0000

    removed unused silex routes

--- src/app.php
+++ src/app.php
@@ -2,25 +2,10 @@
 require_once __DIR__.'/bootstrap.php';
 
 $app->get('/', function() use($app) {
-	$grid = new BON\Model\MazeModel("The Crystal Maze", 21, 21);
+	$grid = new BeardedOctoNinja\Model\Maze("The Crystal Maze", 21, 21);
     return $app['twig']->render('hello.twig', array(
         'grid' => json_encode($grid->get_grid()),
     ));
 });
-
-$app->get('/maze', function() use($app) {
-	$grid = new BON\Model\MazeModel(31, 31);
-    return json_encode($grid->get_grid());
-});
-
-$app->mount(
-'/new',
-new BON\Route\NewPageControllerProvider()
-);
-
-$app->mount(
-'/incoming',
-new BON\Route\SMSIncomingController()
-);
 
 $app->run(); 
