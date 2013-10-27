f4c81c6bad0d67e2b3888a096122409da7db3782
stephcook22 <stephcook22@gmail.com>
2013-10-27 07:06:08 +0000

    added some headers, see if this works

--- src/BeardedOctoNinja/BonServer.php
+++ src/BeardedOctoNinja/BonServer.php
@@ -52,6 +52,10 @@ class BonServer implements MessageComponentInterface {
             foreach ($this->clients as $client) {
                 if ($from === $client) {
                     // The sender is not the receiver, send to each client connected
+                    header("Access-Control-Allow-Orgin: *");
+                    header("Access-Control-Allow-Methods: *");
+                    header("Content-Type: application/json");
+
                     $client->send(json_encode($response));
                 }
             }
