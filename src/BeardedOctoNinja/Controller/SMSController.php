2addb3727d14e28e4bd2476b3ab2a5a642438663
Qas Ashraf <mail@qasashraf.co.uk>
2013-10-27 08:11:19 +0000

    Added temporary cases to deal with messages coming in, just so Joe has some movement messages on the UI

--- src/BeardedOctoNinja/Controller/SMSController.php
+++ src/BeardedOctoNinja/Controller/SMSController.php
@@ -7,8 +7,26 @@ class SMSController {
     {
         switch(strtolower($request['type']))
         {
+            case 'outgoing':
+                return array(
+                    'operation' => 'SMS',
+                    'type' => 'outgoing',
+                    'recipient' => $request['to'],
+                    'message' => $request['content'],
+                    'sender' => $request['from']
+                ); // TODO: Maybe change this format, depending on what's easier for SMS Interceptor
+                break;
+            case 'incoming':
+                return array(
+                  'operation' => 'PLAYER',
+                  'type' => 'move',
+                  'name' => $request['from'],
+                  'x' => 2,
+                  'y' => 0
+                ); // TODO: Process either player movement or...process joining/leaving game
+                break;
             default:
-                return NULL; // Stub 
+                return NULL;
                 break;
         }
     }
